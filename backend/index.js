de9c83ad3d6fd2a129a84cd331480bce53c64b2e
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-02-15 23:13:41 +0530

    Backend_done

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { connection } = require('./Config/db');
 require('dotenv').config()
 const cors = require('cors');
+const { userRouter } = require('./Routes/UserRouter');
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use('/user', userRouter)
 
 app.listen(process.env.PORT, async()=>{
     try {
