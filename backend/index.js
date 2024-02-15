0e8c6fe0e5bc27841f3606276ab2c6dd0f24321c
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-02-15 18:41:57 +0530

    P

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { connection } = require('./Config/db');
+require('dotenv').config()
+const cors = require('cors');
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT, async()=>{
+    try {
+        console.log('connectig to DB');
+        await connection;
+        console.log(`server is running at port ${process.env.PORT}`);
+    } catch (error) {
+        console.log('something went wrong');
+        console.log(error);
+    }
+
+})
